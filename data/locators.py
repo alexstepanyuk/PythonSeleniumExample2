241a464487f25ca7f10cc2ccd9acfa6da38796da
StepBez <stepan26062006@gmail.com>
2023-04-28 19:07:14 +0700

    1

--- data/locators.py
+++ data/locators.py
@@ -6,6 +6,14 @@ class SearchPageLocators:
     SEARCH_BUTTON = (By.XPATH, "//*[@id='search_button_homepage']")
     RESULTS = (By.XPATH, "//*[@id='links']//*[@data-testid='result']")
 
+class TextBoxLocators():
+    TB1=(By.XPATH, '//*[@id="userName"]')
+    TB2 = (By.XPATH, '//*[@id="userEmail"]')
+    TB3 = (By.XPATH, '//*[@id="currentAddress"]')
+    TB4 = (By.XPATH, '//*[@id="permanentAddress"]')
+    B1 = (By.XPATH, '//*[@id="submit"]')
+    RESULTS = (By.XPATH, '//*[@id="output"]/div')
+
 class TextBoxLocators:
     TB1 = (By.XPATH, "//*[@id='userName']")
     TB2 = (By.XPATH, "//*[@id='userEmail']")
