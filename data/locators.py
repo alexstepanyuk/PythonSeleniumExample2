160895312feca82f9d3e8e7d3f97e9c07a726000
StepBez <stepan26062006@gmail.com>
2023-04-28 18:17:58 +0700

    1

--- data/locators.py
+++ data/locators.py
@@ -5,3 +5,11 @@ class SearchPageLocators:
     SEARCH_INPUT = (By.XPATH, "//*[@id='search_form_input_homepage']")
     SEARCH_BUTTON = (By.XPATH, "//*[@id='search_button_homepage']")
     RESULTS = (By.XPATH, "//*[@id='links']//*[@data-testid='result']")
+
+class TextBoxLocators:
+    TB1 = (By.XPATH, "//*[@id='userName']")
+    TB2 = (By.XPATH, "//*[@id='userEmail']")
+    TB3 = (By.XPATH, "//*[@id='currentAddress']")
+    TB4 = (By.XPATH, "//*[@id='permanentAddress']")
+    B1 = (By.XPATH, "//*[@id='submit']")
+    RESULTS = (By.XPATH, '//*[@id="output"]/div')
--- tests/textbox_test.py
+++ tests/textbox_test.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 import pytest
-from pages.search_page import SearchPage
+from pages.textbox_page import TextBoxPage
 from tests.base_test import BaseTest
 
 
@@ -8,11 +8,11 @@ class TestSearch(BaseTest):
 
     @pytest.fixture
     def load_pages(self):
-        self.page = SearchPage(self.driver, self.wait)
+        self.page = TextBoxPage(self.driver, self.wait)
         self.page.go_to_search_page()
 
     def test_title(self, load_pages):
         self.page.check_title("DuckDuckGo — Максимум конфиденциальности, минимум усилий.")
 
     def test_search(self, load_pages):
-        self.page.make_a_search("Selenium")
+        self.page.make_a_search("asdfrfg","sdfghj@asdfg.com","sdfghyjkihngbfdres","asdcfvghyjuik")
