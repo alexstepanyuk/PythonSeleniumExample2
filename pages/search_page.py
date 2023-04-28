503e3e5d8c103a4964cbd678e93e8e3d7c4f1285
Face Left <face-left@mail.ru>
2023-04-28 18:59:57 +0700

    разделил по файлам

--- pages/search_page.py
+++ pages/search_page.py
@@ -1,10 +1,8 @@
 from selenium.webdriver.support import expected_conditions as EC
 from pages.base_page import BasePage
-from data.locators import *
-from time import sleep
+from data.locators import SearchPageLocators
 
 class SearchPage(BasePage):
-
     def __init__(self, driver, wait):
         self.url = "https://duckduckgo.com/"
         self.locator = SearchPageLocators
@@ -22,30 +20,3 @@ class SearchPage(BasePage):
         self.wait.until(EC.presence_of_element_located(self.locator.RESULTS))
         self.driver.save_screenshot("results/results.png")
 
-
-class DemoqaRadioButtonPage(BasePage):
-
-    def __init__(self, driver, wait):
-        self.url = "https://demoqa.com/radio-button"
-        self.locator = DemoqaRadioButtonPageLocators
-        super().__init__(driver, wait)
-
-    def go_to_search_page(self):
-        self.go_to_page(self.url)
-
-    def check_yes_radio(self):
-        self.driver.find_element(*self.locator.YES_TEXT).click()
-        sleep(1)
-        res = self.driver.find_element(*self.locator.RESULTS).text
-        assert self.driver.find_element(*self.locator.YES_RADIO).is_selected() and res == "Yes"
-
-    def check_imp_radio(self):
-        self.driver.find_element(*self.locator.IMP_TEXT).click()
-        sleep(1)
-        res = self.driver.find_element(*self.locator.RESULTS).text
-        assert self.driver.find_element(*self.locator.IMP_RADIO).is_selected() and res == "Impressive"
-
-    def check_no_radio(self):
-        self.driver.find_element(*self.locator.NO_TEXT).click()
-        sleep(1)
-        assert not self.driver.find_element(*self.locator.NO_RADIO).is_selected()
