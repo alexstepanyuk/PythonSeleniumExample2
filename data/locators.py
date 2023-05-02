6e56cb452568304dc219c93d5cdcf61419bfcf99
StepBez <56303897+StepBez@users.noreply.github.com>
2023-05-02 19:55:51 +0700

    Update locators.py

--- data/locators.py
+++ data/locators.py
@@ -5,6 +5,18 @@ class SearchPageLocators:
     SEARCH_INPUT = (By.XPATH, "//*[@id='search_form_input_homepage']")
     SEARCH_BUTTON = (By.XPATH, "//*[@id='search_button_homepage']")
     RESULTS = (By.XPATH, "//*[@id='links']//*[@data-testid='result']")
+    
+class DemoqaRadioButtonPageLocators:
+
+    RESULTS = (By.XPATH, "//*[@class='text-success']")
+
+    YES_TEXT = (By.XPATH, "//*[@for='yesRadio']")
+    IMP_TEXT = (By.XPATH, "//*[@for='impressiveRadio']")
+    NO_TEXT = (By.XPATH, "//*[@for='noRadio']")
+
+    YES_RADIO = (By.XPATH, "//*[@id='yesRadio']")
+    IMP_RADIO = (By.XPATH, "//*[@id='impressiveRadio']")
+    NO_RADIO = (By.XPATH, "//*[@id='noRadio']")
 
 class TextBoxLocators():
     TB1=(By.XPATH, '//*[@id="userName"]')
